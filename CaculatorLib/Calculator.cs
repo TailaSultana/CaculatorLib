6f96b0a6776f975cd41c58f0d665ff5d6e84d079
TailaSultana <taila.sultana@gmail.com>
2021-12-22 17:17:45 +0500

    Initial commit

---
+++ CaculatorLib/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaculatorLib
+{
+    public static class Calculator
+    {
+        public static int? Calc(int n1, int n2, char op)
+        {
+            int? result = null;
+            try
+            {
+                if (op == '+')
+                {
+                    result = n1 + n2;
+                }
+                else if (op == '-')
+                {
+                    result = n1 - n2;
+                }
+                else if (op == '*')
+                {
+                    result = n1 * n2;
+                }
+                else if (op == '/')
+                {
+                    result = n1 / n2;
+                }
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine("Division by zero is not possible");
+            }
+            return result;
+        }
+    }
+}
---
+++ UseCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace UseCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter operator");
+            char op = Console.ReadLine().ToCharArray()[0];
+            int? result = CaculatorLib.Calculator.Calc(num1, num2, op);
+            if (result.HasValue)
+            {
+                Console.WriteLine($"\n{num1} {op}  {num2} = {result}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
